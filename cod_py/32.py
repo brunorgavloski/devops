662d8ecad826d314cc1cb64b734008472e7d220a
Bruno <gavloski11@gmail..com>
2025-05-28 22:06:46 -0300

    32

---
+++ cod_py/32.py
@@ -0,0 +1,34 @@
+if True:
+    print('Its true')
+
+hungry = True
+
+if hungry:
+    print('Its true')
+
+hungry2 = False
+
+if hungry2:
+    print('FEED ME!')
+else:
+    print('Not hungry')
+
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Cars area cool!')
+elif loc == 'Bank':
+    print('Welcome to the store!')
+else:
+    print('I do not know much')
+   
+name = 'Sammy'
+
+if name == 'Bruno':
+    print('Hello Bruno')
+
+elif name == 'Sammy':
+          print('Hello Sammy')
+else:
+    print('What is your name?')
