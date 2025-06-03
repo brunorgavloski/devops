5f513c6575bc06c7543bb565f9290834f2d0b1d8
Bruno <gavloski11@gmail..com>
2025-06-02 21:28:34 -0300

    4.py

---
+++ cod_py/4.py
@@ -0,0 +1,47 @@
+mylist = [1,2,3,4,5,6,7,8,10]
+
+'''
+for num in range(0,11,2):
+    print(num)
+
+index_count = 0
+
+for letter in 'abcde':
+    print('at index {} the letter is {}'.format(index_count, letter))
+
+
+word = 'abcdefghijklmnopqrst'
+
+for item in enumerate(word):
+    print(item)
+
+teste
+
+
+mylist1 = [1, 2, 3, 4, 5]
+mylist2 = ['a', 'b', 'c']
+mylist3 = [100, 200, 300]
+
+for item in zip(mylist1, mylist2, mylist3):
+    print(item)
+'''
+
+from random import shuffle
+from random import randint
+
+mylist1 = [1, 2, 3, 4, 5]
+
+a = randint(0, 100)
+print(a)
+
+result = input('Enter a text')
+print(result)
+print(type(result))
+
+
+
+
+
+
+
+
